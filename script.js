b3afc1adff14b928706bbae09cdb7e6aa1fc7edc
Mukund Narayan <mukundnarayan87@gmail.com>
2025-10-21 19:32:18 +0530

    Fix the header

--- script.js
+++ script.js
@@ -6,3 +6,19 @@ document.querySelectorAll("a[href^='#']").forEach(anchor => {
       .scrollIntoView({ behavior: "smooth" });
   });
 });
+
+// Hamburger menu toggle
+const menuToggle = document.getElementById('menu-toggle');
+const navLinks = document.querySelector('.nav-links ul');
+
+menuToggle.addEventListener('click', () => {
+  navLinks.classList.toggle('show');
+});
+
+// Close hamburger menu when a link is clicked
+const navItems = document.querySelectorAll('.nav-links ul li a');
+navItems.forEach(item => {
+  item.addEventListener('click', () => {
+    navLinks.classList.remove('show');
+  });
+});
